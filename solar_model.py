ddac4e0e43d88f0a5e261d53d1eab5a9e5ce1f04
Hoodyman1 <frosty123dyusha@gmail.com>
2021-11-14 13:10:58 +0300

    Модернезирована функция move_space_object, теперь эллипс статичен в течении некоторого времени

--- solar_model.py
+++ solar_model.py
@@ -33,15 +33,36 @@ def move_space_object(body, dt):
 
     **body** — тело, которое нужно переместить.
     """
+    # запоминаем начальные координаты тела
+    x1 = body.x
+    y1 = body.y
 
+    # рассчитываем ускорение до перемещения
     if body.m != 0:
-        ax = body.Fx/body.m
-        ay = body.Fy/body.m
+        ax1 = body.Fx/body.m
+        ay1 = body.Fy/body.m
     else:
-        ax = ay = 0
-    
-    body.x += body.Vx*dt
-    body.y += body.Vy*dt
+        ax1 = ay1 = 0
+
+    # оценочно рассчитываем положение планеты через dt
+    body.x += body.Vx*dt + ax1 * dt**2 / 2
+    body.y += body.Vy*dt + ay1 * dt**2 / 2
+
+    # рассчитываем ускорение после перемещения
+    if body.m != 0:
+        ax2 = body.Fx/body.m
+        ay2 = body.Fy/body.m
+    else:
+        ax2 = ay2 = 0
+
+    # берем среднее ускорение между начальным и оценочным конечным положениями
+    ax = (ax1 + ax2) / 2
+    ay = (ay1 + ay2) / 2
+
+    # "точные" конечные координаты планеты (начальное положение + среднаяя скорость * dt + среднее ускорение * dt**2)
+    body.x = x1 + dt * (body.Vx + (body.Vx + ax * dt)) / 2 + ax * dt**2 / 2
+    body.y = y1 + dt * (body.Vy + (body.Vy + ay * dt)) / 2 + ay * dt**2 / 2
+
     body.Vx += ax*dt
     body.Vy += ay*dt
 
